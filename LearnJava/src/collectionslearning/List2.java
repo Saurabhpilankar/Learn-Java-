30769cf6ab65c3ae94d64a98dc4ac85f1f1c9488
saurabhpilankar <saurabhpilankar@gmail.com>
2022-12-28 16:23:33 +0530

    program to add integers to list using loop

---
+++ LearnJava/src/collectionslearning/List2.java
@@ -0,0 +1,20 @@
+package collectionslearning;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class List2 {
+
+	public static void main(String[] args) {
+		List<Integer> strinlist = new ArrayList<Integer>();
+		for (int i = 20; i>=1; i--) {
+			strinlist.add(i);
+		}
+		System.out.println(strinlist);
+		//Collections.sort(strinlist);
+		//System.out.println(strinlist);
+
+	}
+
+}
